77d5a2ddfa2d075cc1179fff43157b6b5a77bb34
Usharaj09 <99117412+Usharaj09@users.noreply.github.com>
2022-03-30 22:46:32 -0700

    Update set1.sql

--- Telecomservice_proj/set1.sql
+++ Telecomservice_proj/set1.sql
@@ -1,10 +1,16 @@
-SELECT count(*) AS numbers_of_customers,product from telecom_customer group by product;
+SELECT count(*) AS numbers_of_customers,product 
+from telecom_customer
+group by product;
 
-select customer_name,service_segment from telecom_customer;
+select customer_name,service_segment
+from telecom_customer;
 
-select count(*) from telecom_customer where customer_segment='Consumer';
+select count(*) from telecom_customer
+where customer_segment='Consumer';
 
-select customer_name from telecom_customer WHERE product='Digital Subscriber Line';
+select customer_name
+from telecom_customer
+WHERE product='Digital Subscriber Line';
 
 select customer_id,customer_name from telecom_customer where customer_name like'sa%';
 
